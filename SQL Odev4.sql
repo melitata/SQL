4de4a1746f12652e66c79bbd16dd9aad8a311d07
Melisa Kılıç <109312556+melitata@users.noreply.github.com>
2024-11-12 19:26:08 +0300

    Add files via upload

---
+++ SQL Odev4.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT replacement_cost  FROM film;
+
+SELECT COUNT (DISTINCT replacement_cost)  FROM film;
+
+SELECT title FROM film
+COUNT WHERE title LIKE 'T%' AND rating='G';
+
+SELECT country FROM country
+COUNT WHERE country LIKE '_____';
+
+SELECT COUNT(*)
+FROM city
+WHERE LOWER(city) LIKE '%r';
+
